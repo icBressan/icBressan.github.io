dcbe2de394f79539f77fdef06df4b462d2ed8835
Isabelle <86361001+icBressan@users.noreply.github.com>
2024-08-08 17:05:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function loadContent(url) {
+
   if (url === 'resumo') {
+    // URL do currículo 
     const urlcv = "https://drive.google.com/uc?export=download&id=1k7Eftoj9L7NMeiPe8aXRnXYPTepGlwpU";
     const pdfcv = "src/curriculo.pdf"
     const div = document.getElementById("contentstuff");
     div.innerHTML = `
-      <center><a class="buttond" href="${urlcv}" target="_blank"> Download CV </a></center>
-<iframe src="${pdfcv}" width="100%" height="450px"></iframe>
+      <center>
+      <a class="buttond" href="${urlcv}" target="_blank"> Download </a>
+      </center>
+      <iframe src="${pdfcv}" width="100%" height="450px"></iframe>
     `
   }
 
+  //carrega o conteúdo da página HTML
   else {
     fetch(url)
       .then(response => response.text())
@@ -17,4 +22,3 @@ function loadContent(url) {
       });
   }
 }
-
